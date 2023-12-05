4a29995dc27e85714de164b0631a1fa35ea5d9eb
bz-888 <benjamin.zheng95@gmail.com>
2023-12-05 10:01:41 -0600

    challenge 1

---
+++ python-functions.py
@@ -0,0 +1,12 @@
+# =================
+# Challenge 1
+# =================
+
+def sum_to(arg):
+    for num in range(arg+1):
+        sum += num
+
+    return sum
+
+print(f"sum_to(6) = {sum_to(6)}"
+print(f"sum_to(10) = {sum_to(10)}")    
